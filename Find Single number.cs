048c85c744cff176eec212a9a97d94a987eef9b9
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-10 02:59:49 +0530

    Add project files.

---
+++ Find Single number.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pettern_program
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Input: nums = [2,2,1]
+            //Output: 1
+            int temp = 0;
+            int k = 3;
+            int[] num = { 2,2,1 };
+            for (int i = 1; i < num.Length; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if (i != j)
+                    {
+
+                        temp = i;
+
+                    }
+
+                        {
+                            Console.Write(" single number  = " + temp);
+                        }
+                    Console.ReadLine();
+                }
+                }
+            }
+        }
+    }
+
+
+
+
+    
+
+
+            
